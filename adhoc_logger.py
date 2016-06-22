57c97e28ff2e7ff32ee4be534a36132bfe8eec8b
bevensteven <struong@berkeley.edu>
2016-06-22 16:39:33 -0700

    adhoc logger

---
+++ adhoc_logger.py
@@ -0,0 +1,31 @@
+import os
+
+class adHocLogger(object):
+	"""
+	Substitute for MozDefMsg instane used netfilter_openvpn.
+	Implements the same functions used in the script; only difference being that messages are logged to a designated log file instead.
+	"""
+	def __init__(self, filepath='~/netfilter_openvpn.log', tags=list()):
+		super(adHocLogger, self).__init__()
+		self.filepath = filepath
+		self.tags = tags
+		if os.path.isfile(filepath):
+			print("Initializing a new log. Removing {}".format(filepath))
+			os.remove(filepath)
+		self.lf = open(filepath, 'a')
+		self.lf.write('TAGS: {}'.format(', '.join(tags)))
+		self.line = 0 
+
+	def sendToSyslog(self, boolean):
+		self.lf.write('sendToSyslog = {}'.format(str(boolean)))
+
+	def syslogOnly(self, boolean):
+		self.lf.write('syslogOnly = {}'.format(str(boolean)))
+
+	def send(self, summary=str(), details=dict()):
+		self.lf.write('[ SUMMARY ] line {} | {}\n'.format(self.line, summary))
+		self.lf.write('[ DETAILS ] line {}\n'.format(self.line))
+		for key in details:
+			self.lf.write('[ DETAILS ] ({}) {}: {}\n'.format(self.line, str(key), str(details[key])))
+		self.line += 1
+		self.lf.write('\n')

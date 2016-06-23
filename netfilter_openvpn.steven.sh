232be11b8c8b75d68c1b141384da8ea95b5333c2
Steven Truong <struong@berkeley.edu>
2016-06-23 17:56:48 +0000

    modded netfilter scripts for VPN

--- netfilter_openvpn.steven.sh
+++ netfilter_openvpn.steven.sh
@@ -8,18 +8,26 @@ address=$2
 #common_name=${username}
 export operation address common_name
 
+
+echo "LEARN-ADDRESS SCRIPT IS BEING CALLED" >> /etc/openvpn/logs/connection.log
+echo "OPERATION: ${operation}" >> /etc/openvpn/logs/connection.log
+echo "ADDRESS: ${address}" >> /etc/openvpn/logs/connection.log
+echo "CN: ${common_name}" >> /etc/openvpn/logs/connection.log
+
 SUDO=/usr/bin/sudo
 IPTABLES=/sbin/iptables
 
 if [[ ${operation} == "delete" ]]; then
 	${SUDO} ${IPTABLES} -D INPUT -s ${address} -j DROP
 else
+	echo "running cmd: ${SUDO} ${IPTABLES} -I INPUT -s ${address} -j DROP" >> /etc/openvpn/logs/connection.log
 	${SUDO} ${IPTABLES} -I INPUT -s ${address} -j DROP || {
 		echo "Failed to run initial iptables command"
 		exit 127
 	}
 fi
 
-${SUDO} /usr/lib/openvpn/plugins/netfilter_openvpn.steven.py ${operation} &
+echo "\n" >> /etc/openvpn/logs/connection.log
+${SUDO} python /usr/lib/openvpn/plugins/netfilter_openvpn.steven.py ${operation} &
 disown
 exit 0
